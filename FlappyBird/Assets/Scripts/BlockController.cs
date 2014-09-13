1458767994153b2ccef9b4f18e68a16cf71f0695
Guo Dong <smile_guodong@163.com>
2014-09-13 18:00:31 +0800

    add GrandmaVSZombie

---
+++ FlappyBird/Assets/Scripts/BlockController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlockController : MonoBehaviour {
+
+	public float min_boundary;
+	public float max_boundary;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ GrandmaVSZombie/Assets/Scripts/ZombieAnimator.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ZombieAnimator : MonoBehaviour {
+
+	public Sprite[] frames;
+	public float frames_per_second;
+	private SpriteRenderer sprite_renderer;
+	private float delta_time_;
+	private int	  frame_index = 0;
+	// Use this for initialization
+	void Start () {
+		frames_per_second = Mathf.Abs(frames_per_second);
+		delta_time_ = 0.0f;
+		sprite_renderer = renderer as SpriteRenderer;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		delta_time_ += Time.deltaTime;
+		if ( delta_time_ > frames_per_second ) 
+		{
+			delta_time_ -= frames_per_second;
+			frame_index++;
+			frame_index %= frames.Length;
+			sprite_renderer.sprite = frames[frame_index];
+		}
+	}
+}
